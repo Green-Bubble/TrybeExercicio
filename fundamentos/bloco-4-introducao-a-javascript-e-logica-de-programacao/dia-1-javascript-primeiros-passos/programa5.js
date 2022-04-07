2e25d4411a772805a306f2fc1295a30695ecbef8
FoReVeR <forever@github.com>
2022-04-06 23:23:15 -0300

    Adicionei programa5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa5.js
@@ -0,0 +1,13 @@
+/*const angulo1 = 60;
+const angulo2 = 60;
+const angulo3 = 60;*/
+
+let angulo1 = 60;
+let angulo2 = 60;
+let angulo3 = 60;
+
+if (angulo1 + angulo2 + angulo3 == 180) {
+    console.log(true);
+} else if (!(angulo1 + angulo2 + angulo3 == 180)) {
+    console.log(false);
+} 
