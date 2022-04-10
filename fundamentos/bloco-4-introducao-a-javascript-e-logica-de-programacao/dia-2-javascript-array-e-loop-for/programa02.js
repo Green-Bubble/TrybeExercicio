8ef6bbb385ef4cee9ac59951a4a59862037a1719
FoReVeR <forever@github.com>
2022-04-10 15:43:01 -0300

    Modifiquei o For

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/programa02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/programa02.js
@@ -1,3 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
 
-for ()
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index]; 
+}
+
+console.log(soma);
