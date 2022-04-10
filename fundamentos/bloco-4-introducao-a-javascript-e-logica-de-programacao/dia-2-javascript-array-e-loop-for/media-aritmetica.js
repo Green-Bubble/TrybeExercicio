278e89c65e8829dab713c36e5dd5a951b691d1da
FoReVeR <forever@github.com>
2022-04-10 16:03:21 -0300

    Modifiquei a expressão que calcula a média

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/media-aritmetica.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/media-aritmetica.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-    media += numbers[index] / 10; 
+    media += numbers[index] / numbers.length; 
 }
 
 console.log(media);
