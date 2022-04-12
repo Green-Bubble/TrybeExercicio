1cd6a2bcdb0e73ede0284eca04d95afb6dd51324
FoReVeR <forever@github.com>
2022-04-12 10:26:26 -0300

    Adicionei um novo objeto e condição e console log.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/margarida.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/margarida.js
@@ -3,6 +3,12 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
+    info2: {
+        personagem: 'Tio Patinhas',
+        origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+        nota: 'O último MacPatinhas',
+        recorrente: 'Sim',
+    },
   };
 
   //console.log('Bem-vinda ' + info.personagem);
@@ -12,7 +18,19 @@ let info = {
     console.log(key);
   }*/
 
-  for(let value in info) {
+  /*for(let value in info) {
       console.log(info[value]);
+  }*/
+
+  console.log(info.personagem + ' e ' + info.info2.personagem);
+  console.log(info.origem + ' e ' + info.info2.origem);
+  console.log(info.nota + ' e ' + info.info2.nota);
+
+  if(info.recorrente == 'não') {
+      console.log(inválido);
+  } else if (info.info2.recorrente == 'não'){
+      console.log(inválido);
+  } else {
+      console.log('Ambos recorrentes');
   }
   
