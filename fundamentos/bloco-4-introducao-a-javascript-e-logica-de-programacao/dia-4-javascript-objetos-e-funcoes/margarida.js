1cdacb1195ed287aaec91dcf9699414f43e664b3
FoReVeR <forever@github.com>
2022-04-12 09:28:54 -0300

    Adicionei a propriedade recorrente e o valor sim

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/margarida.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/margarida.js
@@ -2,6 +2,8 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
   };
 
-  console.log('Bem-vinda ' + info.personagem);
+  //console.log('Bem-vinda ' + info.personagem);
+  console.log(info);
