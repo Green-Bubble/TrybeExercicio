b9b729dfb3b3619767b26f123153ef196f2309b1
FoReVeR <forever@github.com>
2023-01-13 16:09:22 -0300

    Exercício 1.

---
+++ back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-2-Heranca-e-Composicao/index.ts
@@ -0,0 +1,7 @@
+import Person from './Person';
+
+const maria = new Person('Maria da Consolação', new Date('1980/01/25'));
+const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
+
+console.log(maria);
+console.log(luiza);
