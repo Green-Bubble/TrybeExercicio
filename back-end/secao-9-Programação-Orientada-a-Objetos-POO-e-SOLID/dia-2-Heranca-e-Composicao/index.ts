a2fb0465e8aa540a8e93c04548c477fb38cf56d8
FoReVeR <forever@github.com>
2023-01-15 09:58:00 -0300

    Exercício 2.

--- back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-2-Heranca-e-Composicao/index.ts
+++ back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-2-Heranca-e-Composicao/index.ts
@@ -1,3 +1,5 @@
+//Para testar - Exercício 1:
+
 import Person from './Person';
 
 const maria = new Person('Maria da Consolação', new Date('1980/01/25'));
@@ -5,3 +7,24 @@ const luiza = new Person('Luiza Andrade', new Date('2005/10/02'));
 
 console.log(maria);
 console.log(luiza);
+
+// Para testar - Exercício 2:
+
+import Student from './Student';
+
+const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
+const lucas = new Student('Lucas Peixoto Salgueiro', new Date('2006/07/19'));
+const jessica = new Student('Jéssica Bianca Nunes', new Date('2004/06/06'));
+const tamires = new Student('Tamires Santos Bastos', new Date('2005/11/05'));
+const fernando = new Student('Fernando Gonçalves', new Date('2006/09/11'));
+
+carolina.examsGrades = [25, 20, 25, 23];
+lucas.examsGrades = [25, 20, 25, 23];
+jessica.assignmentsGrades = [50, 45];
+tamires.assignmentsGrades = [47, 42];
+
+console.log(carolina);
+console.log(lucas);
+console.log(jessica);
+console.log(tamires);
+console.log(fernando);
