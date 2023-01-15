7b40667761360af70f3b8db8b23e7d99a70a0feb
FoReVeR <forever@github.com>
2023-01-15 18:08:49 -0300

    Exercício 2.

--- back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-2-Heranca-e-Composicao/index.ts
+++ back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-2-Heranca-e-Composicao/index.ts
@@ -31,3 +31,21 @@ console.log(fernando);
 
 console.log(carolina.sumAverageGrade());
 console.log(lucas.sumAverageGrade());
+
+// Para testar - Exercício 3:
+
+import Employee from './Employee';
+
+const testInterfaceEmployee: Employee = {
+    registration: 'FNC1234567891011',
+    salary: 1200.00,
+    admissionDate: new Date(),
+
+    generateRegistration(): string {
+        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+        return `FNC${randomStr}`;
+    },
+}
+
+console.log((testInterfaceEmployee));
