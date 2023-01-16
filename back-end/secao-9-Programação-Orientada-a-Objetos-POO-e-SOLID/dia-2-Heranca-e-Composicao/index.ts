56c1d9bf523061d1ba78987d42aaaf7d660c8ba7
FoReVeR <forever@github.com>
2023-01-15 22:08:56 -0300

    Exercício 5.

--- back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-2-Heranca-e-Composicao/index.ts
+++ back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-2-Heranca-e-Composicao/index.ts
@@ -52,12 +52,29 @@ console.log((testInterfaceEmployee));
 
 // Para testar - Exercício 4:
 
+// import Subject from './Subject';
+
+// const math = new Subject('Matemática');
+// const story = new Subject('História');
+// const philosophy = new Subject('Filosofia');
+
+// console.log(math);
+// console.log(story);
+// console.log(philosophy);
+
+// Para testar - Exercício 5:
+
 import Subject from './Subject';
+import Teacher from './Teacher';
 
 const math = new Subject('Matemática');
-const story = new Subject('História');
+const history = new Subject('História');
 const philosophy = new Subject('Filosofia');
 
-console.log(math);
-console.log(story);
-console.log(philosophy);
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, history);
+const lucio = new Teacher('Lucio Teixeira', new Date('1986/01/29'), 2000, philosophy);
+
+console.log(marta);
+console.log(joao);
+console.log(lucio);
