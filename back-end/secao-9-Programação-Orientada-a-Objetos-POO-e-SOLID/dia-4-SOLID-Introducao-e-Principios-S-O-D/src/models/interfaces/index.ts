1816a0638b61e61b2fbf79417a0d50d487613aea
FoReVeR <forever@github.com>
2023-01-18 09:28:11 -0300

    Implementação da Model.

--- back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-4-SOLID-Introducao-e-Principios-S-O-D/src/models/interfaces/index.ts
+++ back-end/secao-9-Programação-Orientada-a-Objetos-POO-e-SOLID/dia-4-SOLID-Introducao-e-Principios-S-O-D/src/models/interfaces/index.ts
@@ -1,1 +1,8 @@
 // export { IPlant, InewPlant } from './IPlant'
+export {
+    IModel,
+    IModelReader,
+    IModelWriter,
+    IModelDelete,
+} from './IModel'
+export { default as IOpsInfo } from './OpsInfo';
