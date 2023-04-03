845468cee10318325d77c035c4335d75145f2116
FoReVeR <forever@github.com>
2023-04-03 16:22:53 -0300

    feat: padrão Decorator e log colorido.

---
+++ ciencia-da-computacao/secao-2-padroes-de-projeto/dia-3-padroes-decorator-observer-factory/cores.py
@@ -0,0 +1,13 @@
+ROSA = "\033[95m"
+ROXO = "\033[94m"
+AZUL = "\033[96m"
+VERDE = "\033[92m"
+LARANJA = "\033[93m"
+VERMELHO = "\033[91m"
+NEGRITO = "\033[1m"
+SUBLINHADO = "\033[4m"
+RESET = "\033[0m"
+
+print("\033[94mOlá Mundo!\033[0m")
+# ou
+print(f"{ROSA}Olá Mundo!{RESET}")
