2215b264988a39bcdb0fe03d8f18ede9614aec40
FoReVeR <forever@github.com>
2023-04-24 16:14:44 -0300

    feat: algoritmo recursivo que encontra maior número.

---
+++ ciencia-da-computacao/secao-4-algoritmos/dia-2-recursividade-e-estrategias-para-solucao-de-problemas/exercicio3.py
@@ -0,0 +1,20 @@
+# Crie um algoritmo recursivo que encontre, em uma lista, o maior número
+# inteiro.
+
+def maiorinteiro_aux(lista, tamanho):
+    if tamanho == 1:
+        return lista[0]
+    else:
+        maior_do_resto_da_lista = maiorinteiro_aux(lista, tamanho-1)
+        if maior_do_resto_da_lista > lista[tamanho-1]:
+            return maior_do_resto_da_lista
+        else:
+            return lista[tamanho-1]
+
+
+def maiorinteiro(lista):
+    tamanho = len(lista)
+    return maiorinteiro_aux(lista, tamanho)
+
+
+print(maiorinteiro([1, 21, 300, 4, 57]))
